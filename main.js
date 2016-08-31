28d02a64dd661e5b39baa42c44a805204eb88349
camilin87 <camilin87@gmail.com>
2016-08-31 18:27:54 -0400

    read the aws credentials from environment variables

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 console.log("logging something hell yeah!", new Date());
+
+console.log("AWS_ACCESS_KEY_ID", process.env.AWS_ACCESS_KEY_ID);
+console.log("AWS_SECRET_ACCESS_KEY", process.env.AWS_SECRET_ACCESS_KEY);
+
