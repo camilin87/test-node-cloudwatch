8c3043e8fe325408f34777e30a8f249320d4c2fd
camilin87 <camilin87@gmail.com>
2016-08-31 18:30:34 -0400

    configure the aws credentials on launch

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-console.log("logging something hell yeah!", new Date());
+var AWS = require('aws-sdk');
 
-console.log("AWS_ACCESS_KEY_ID", process.env.AWS_ACCESS_KEY_ID);
-console.log("AWS_SECRET_ACCESS_KEY", process.env.AWS_SECRET_ACCESS_KEY);
+// console.log("AWS_ACCESS_KEY_ID", process.env.AWS_ACCESS_KEY_ID);
+// console.log("AWS_SECRET_ACCESS_KEY", process.env.AWS_SECRET_ACCESS_KEY);
+
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: 'secret'
+});
+
+console.log("logging something hell yeah!", new Date());
 
