83d6025724cf2e86173bafc8310e989c91b519c1
camilin87 <camilin87@gmail.com>
2016-09-03 10:50:47 -0400

    add more logs

--- simple-writer.js
+++ simple-writer.js
@@ -2,10 +2,10 @@ console.log("starting...", new Date());
 
 console.log("AWS_REGION", process.env.AWS_REGION)
 
-for (var i = 0; i < 10000; i++){
+for (var i = 0; i < 1000000; i++){
     console.log("logging ", i);
 }
 
 console.log("finishing...", new Date());
 
-throw new Error("unexpected");
+// throw new Error("unexpected");
