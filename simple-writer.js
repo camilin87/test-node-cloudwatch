3a86f102e23823a1eb016eaa0cf0a5aeb082176c
camilin87 <camilin87@gmail.com>
2016-09-04 00:21:52 -0400

    use the redirector

--- simple-writer.js
+++ simple-writer.js
@@ -2,10 +2,18 @@ console.log("starting...", new Date());
 
 console.log("AWS_REGION", process.env.AWS_REGION)
 
-for (var i = 0; i < 1000000; i++){
-    console.log("logging ", i);
+for (var i = 0; i < 100000; i++){
+    console.log("l ", i);
 }
 
-console.log("finishing...", new Date());
+setTimeout(() => {
+
+    for (var i = 0; i < 100000; i++){
+        console.log("f ", i);
+    }
+
+    console.log("finishing...", new Date());
+}, 2000);
+
 
 // throw new Error("unexpected");
