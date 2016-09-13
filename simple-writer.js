b66c1219ad9cc940033243b699bb4b11ddb40487
camilin87 <camilin87@gmail.com>
2016-09-13 08:14:50 -0400

    verify [] are properly written

--- simple-writer.js
+++ simple-writer.js
@@ -5,13 +5,13 @@ console.log("AWS_REGION", process.env.AWS_REGION)
 var max_messages = 100000
 
 for (var i = 0; i < max_messages; i++){
-    console.log("l ", i);
+    console.log("[l] ", i);
 }
 
 setTimeout(() => {
 
     for (var i = 0; i < max_messages; i++){
-        console.log("f ", i);
+        console.log("[f] ", i);
     }
 
     console.log("finishing...", new Date());
