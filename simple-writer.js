d7e7915fd66365a4b721c66fcad13c0dfd8c0424
camilin87 <camilin87@gmail.com>
2016-09-13 08:21:14 -0400

    the forwarder can forward "

--- simple-writer.js
+++ simple-writer.js
@@ -5,7 +5,7 @@ console.log("AWS_REGION", process.env.AWS_REGION)
 var max_messages = 100000
 
 for (var i = 0; i < max_messages; i++){
-    console.log("[l] ", i);
+    console.log("\"[l]\" ", i);
 }
 
 setTimeout(() => {
